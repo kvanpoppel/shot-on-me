6cdc8ce6e4eb6963efcd6aa7011aadcd4e4761db
Kate Van Poppel <kvanpoppel@orthoindy.com>
2025-11-26 11:35:25 -0500

    Convert to Pages Router to reduce serverless functions

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import { AppProps } from 'next/app'
+import { AuthProvider } from '../app/contexts/AuthContext'
+import { GoogleMapsProvider } from '../app/contexts/GoogleMapsContext'
+import { SocketProvider } from '../app/contexts/SocketContext'
+import '../app/globals.css'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <AuthProvider>
+      <GoogleMapsProvider>
+        <SocketProvider>
+          <Component {...pageProps} />
+        </SocketProvider>
+      </GoogleMapsProvider>
+    </AuthProvider>
+  )
+}
