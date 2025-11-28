302de4dbce09d7a607133374d67860fb39796eb9
Kate Van Poppel <kvanpoppel@orthoindy.com>
2025-11-27 23:31:10 -0500

    Add group chat backend - create groups, join by invite code, send messages

---
+++ backend/models/Group.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+
+const groupSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 100
+  },
+  description: {
+    type: String,
+    trim: true,
+    maxlength: 500
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  members: [{
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'member'],
+      default: 'member'
+    },
+    joinedAt: {
+      type: Date,
+      default: Date.now
+    }
+  }],
+  avatar: {
+    type: String,
+    default: ''
+  },
+  isPrivate: {
+    type: Boolean,
+    default: false
+  },
+  inviteCode: {
+    type: String,
+    unique: true,
+    sparse: true
+  }
+}, {
+  timestamps: true
+});
+
+// Index for efficient querying
+groupSchema.index({ creator: 1 });
+groupSchema.index({ 'members.user': 1 });
+groupSchema.index({ inviteCode: 1 });
+
+// Generate unique invite code
+groupSchema.methods.generateInviteCode = function() {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let code = '';
+  for (let i = 0; i < 8; i++) {
+    code += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  this.inviteCode = code;
+  return code;
+};
+
+module.exports = mongoose.model('Group', groupSchema);
+
