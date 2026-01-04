c7c434b0e98f8efbb303729530fd4ec62fb2e2d1
Kate Van Poppel <kvanpoppel@orthoindy.com>
2026-01-04 17:59:57 -0500

    REVERT: Remove dynamic import - use simple export like before

--- shot-on-me/app/page.tsx
+++ shot-on-me/app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import dynamic from 'next/dynamic'
 import { useAuth } from './contexts/AuthContext'
 import LoginScreen from './components/LoginScreen'
 import Dashboard from './components/Dashboard'
