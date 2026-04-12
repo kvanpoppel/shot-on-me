e37741be79600c3ff0b9aff20c979c9295a78a0a
Kate Van Poppel <kvanpoppel@orthoindy.com>
2026-04-12 00:19:16 -0400

    Fix fizz CSP: remove nonce proxy, pass through clean

--- fizz/proxy.ts
+++ fizz/proxy.ts
@@ -1,29 +1,8 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { randomBytes } from 'crypto'
 
 export function proxy(request: NextRequest) {
-  const nonce = randomBytes(16).toString('base64')
-
-  const cspHeader = [
-    "default-src 'self'",
-    `script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://maps.googleapis.com https://js.stripe.com`,
-    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-    "font-src 'self' https://fonts.gstatic.com",
-    "img-src 'self' data: blob: https://res.cloudinary.com https://*.googleapis.com https://maps.gstatic.com",
-    `connect-src 'self' https://*.googleapis.com https://shot-on-me.onrender.com wss://shot-on-me.onrender.com https://api.stripe.com`,
-    "frame-src https://js.stripe.com",
-    "worker-src 'self' blob:",
-    "manifest-src 'self'",
-  ].join('; ')
-
-  const requestHeaders = new Headers(request.headers)
-  requestHeaders.set('x-nonce', nonce)
-
-  const response = NextResponse.next({ request: { headers: requestHeaders } })
-  response.headers.set('Content-Security-Policy', cspHeader)
-
-  return response
+  return NextResponse.next()
 }
 
 export const config = {
