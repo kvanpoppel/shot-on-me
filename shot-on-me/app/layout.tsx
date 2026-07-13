5b25c07e49ef1080a8cca8ad4aa22f95d5cb5e0e
Kate Van Poppel <kvanpoppel@orthoindy.com>
2026-07-12 21:30:57 -0400

    Switch back to Dancing Script font

--- shot-on-me/app/layout.tsx
+++ shot-on-me/app/layout.tsx
@@ -49,7 +49,7 @@ export default async function RootLayout({
         <meta httpEquiv="Expires" content="0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Inter:wght@300;400;500;600;700;800&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&family=Inter:wght@300;400;500;600;700;800&display=swap" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" sizes="180x180" href="/icon-180x180.png" />
         <link rel="icon" type="image/png" sizes="192x192" href="/icon-192x192.png" />
